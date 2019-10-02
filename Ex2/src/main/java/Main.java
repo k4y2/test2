53446b19a0abb400bc8ebe8331c5bee8ce933369
Tuan Anh Nguyen <kayz.info@gmail.com>
2019-10-02 16:47:31 +0700

    Add ex5

--- Ex2/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        HTMLParser parser = new HTMLParser("http://dantri.com.vn");
-
-        Utils.writeToFile(parser.getHTML());
-    }
-}
